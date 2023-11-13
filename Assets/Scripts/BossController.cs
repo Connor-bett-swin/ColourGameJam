07a609512cee111e6f380124552c1de2e681e0e7
Clive Patrick <clive.m.patrick@gmail.com>
2023-11-13 18:41:35 +1100

    Added import preset

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    private Rigidbody2D m_Rigidbody;
+
+	private void Awake()
+	{
+		m_Rigidbody = GetComponent<Rigidbody2D>();
+	}
+}
