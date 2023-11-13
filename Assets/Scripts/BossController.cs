393674c872929e3c40afbe1500582ffc36288eb8
Clive Patrick <clive.m.patrick@gmail.com>
2023-11-13 20:05:52 +1100

    Added squish

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,19 +14,29 @@ public class BossController : MonoBehaviour
 	private float m_JumpForce = 400;
 	[SerializeField]
 	private float m_CoyoteTime = 0.3f;
+	[SerializeField]
+	private float m_SquishDamping = 0.1f;
+	[SerializeField]
+	private float m_SquishIntensity = 0.2f;
+	[SerializeField]
+	private float m_SquishFromVelocity = 0.1f;
+	[SerializeField]
+	private SpriteRenderer m_SpriteRenderer;
+	[SerializeField]
+	private Animator m_Animator;
+	private float m_Squish;
+	private float m_SquishVelocity;
 	private bool m_Grounded = true;
 	private float m_AirTime;
 	private Vector2 m_Velocity;
 	private ContactPoint2D[] m_Contacts = new ContactPoint2D[8];
     private Rigidbody2D m_Rigidbody;
-	private Animator m_Animator;
 	private PlayerInput m_PlayerInput;
 	private InputAction m_MoveAction;
 
 	private void Awake()
 	{
 		m_Rigidbody = GetComponent<Rigidbody2D>();
-		m_Animator = GetComponent<Animator>();
 		m_PlayerInput = GetComponent<PlayerInput>();
 
 		m_MoveAction = m_PlayerInput.actions["Move"];
@@ -46,13 +57,18 @@ public class BossController : MonoBehaviour
 		var targetVelocity = new Vector2(moveInput * m_MaxSpeed, m_Rigidbody.velocity.y);
 		m_Rigidbody.velocity = Vector2.SmoothDamp(m_Rigidbody.velocity, targetVelocity, ref m_Velocity, m_MovementDamping);
 
+		var targetSquish = math.tanh(m_Rigidbody.velocity.y * m_SquishFromVelocity);
+		m_Squish = Mathf.SmoothDamp(m_Squish, targetSquish, ref m_SquishVelocity, m_SquishDamping);
+		m_SpriteRenderer.transform.localScale = new Vector3(Mathf.LerpUnclamped(1, 1 + m_SquishIntensity, m_Squish),
+			Mathf.LerpUnclamped(1, 1 - m_SquishIntensity, m_Squish), 1);
+
 		if (moveInput > 0.1f)
 		{
-			transform.localScale = new Vector3(-1, 1, 1);
+			m_SpriteRenderer.flipX = true;
 		}
 		else if (moveInput < -0.1f)
 		{
-			transform.localScale = new Vector3(1, 1, 1);
+			m_SpriteRenderer.flipX = false;
 		}
 
 		if (m_Grounded)
