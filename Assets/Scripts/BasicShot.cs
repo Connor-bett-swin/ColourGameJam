4060f23def1f9f0f8a360a2d26a5a7c6353e7661
Connor-bett-swin <103096675@student.swin.edu.au>
2023-11-19 01:01:40 +1100

    Basic Shot weapon

---
+++ Assets/Scripts/BasicShot.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class BasicShot : MonoBehaviour
+{
+    public GameObject SniperArrow;
+    public Transform shootPoint; 
+    public float arrowSpeed = 5f; // Speed of the arrow
+
+    private GameObject player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            ShootArrow();
+            UnityEngine.Debug.Log("44444444444444444444");
+        }
+    }
+
+    private void ShootArrow()
+    {
+        Vector2 targetPosition = player.transform.position;
+
+        GameObject arrow = Instantiate(SniperArrow, shootPoint.position, Quaternion.identity);
+        Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
+        Vector2 direction = (targetPosition - (Vector2)shootPoint.position).normalized;
+        
+        // Calculate the angle and rotate the arrow
+        float angle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) + 90;
+        arrow.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        rb.velocity = direction * arrowSpeed;
+    }
+}
