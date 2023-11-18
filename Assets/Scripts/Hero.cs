c1ac397f4e90233f23cb69e3cb1f4f53b25a4f16
Clive Patrick <clive.m.patrick@gmail.com>
2023-11-19 03:59:13 +1100

    Fixed path following

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -4,17 +4,22 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Hero : MonoBehaviour
 {
     [SerializeField]
     private BehaviorTree m_BehaviorTree;
 	[SerializeField]
+	private float m_JumpCooldown = 0.5f;
+	[SerializeField]
 	private float m_JumpAngle = 45;
 	[SerializeField]
-	private float m_DropAngle = 45;
+	private float m_DropAngle = 60;
 	[SerializeField]
 	private Animator m_Animator;
+	[SerializeField]
+	private Collider2D m_Collider;
 	[SerializeField]
 	private ArrowRainSequence m_ArrowRainSequence;
 	[SerializeField]
@@ -23,7 +28,9 @@ public class Hero : MonoBehaviour
 	private Health m_PlayerHealth;
 	private Seeker m_Seeker;
 	private Character m_Character;
-	private List<Vector2> m_Path;
+	private List<Vector2> m_Path = new List<Vector2>();
+	private Vector2 m_Target;
+	private float m_JumpCooldownTimer;
 
 	private void Awake()
 	{
@@ -38,7 +45,17 @@ public class Hero : MonoBehaviour
 			.Build();
 
 		m_Seeker = GetComponent<Seeker>();
+		m_Seeker.pathCallback = OnPathComplete;
+
+		InvokeRepeating(nameof(UpdatePath), 0, 1.5f);
+
 		m_Character = GetComponent<Character>();
+	}
+
+	private void OnPathComplete(Path path)
+	{
+		m_Path = path.vectorPath.Select(x => (Vector2)x).ToList();
+		m_Path.RemoveAt(0);
 	}
 
 	private void Start()
@@ -46,40 +63,64 @@ public class Hero : MonoBehaviour
 		m_Player = GameObject.FindGameObjectWithTag("Player");
 		m_PlayerHealth = m_Player.GetComponent<Health>();
 
-		StartCoroutine(Seek());
+		//StartCoroutine(Seek());
+	}
+
+	//private PointNode GetFurthestNode()
+	//{
+	//	var pathNodes = AstarPath.active.data.pointGraph.nodes;
+	//	return pathNodes.OrderByDescending(x => Vector2.Distance((Vector3)x.position, m_Player.transform.position)).First();
+	//}
+
+	private PointNode GetFurthestNode()
+	{
+		var pathNodes = AstarPath.active.data.pointGraph.nodes;
+		return pathNodes.OrderByDescending(x => Mathf.Abs(((Vector3)x.position).x - m_Player.transform.position.x) - 3.5f * Mathf.Abs(((Vector3)x.position).y - m_Player.transform.position.y)).First();
 	}
 
 	private void Update()
 	{
-		m_BehaviorTree.Tick();
+		//m_BehaviorTree.Tick();
 
-		m_Character.Move(0);
+		UpdateMovement();
 
-		if (m_Path == null || m_Path.Count == 0)
+		if (m_JumpCooldownTimer > 0)
 		{
+			m_JumpCooldownTimer -= Time.deltaTime;
+		}
+	}
+
+	private void UpdateMovement()
+	{
+		if (m_Path.Count == 0)
+		{
+			var pathNodes = AstarPath.active.data.pointGraph.nodes;
+			m_Target = (Vector3)pathNodes[Random.Range(0, pathNodes.Length)].position;
 			return;
 		}
 
 		var nextNode = m_Path.First();
 
-		if (Vector2.Distance((Vector2)transform.position, nextNode) < 5)
+		if (m_Character.Grounded && m_Collider.OverlapPoint(nextNode))
 		{
 			m_Path.RemoveAt(0);
 		}
 
-		if (!m_Character.Grounded)
+		if (m_Character.Grounded)
 		{
-			return;
+			m_Character.Move(nextNode.x < m_Character.transform.position.x ? -1 : 1);
+		}
+		else
+		{
+			m_Character.Move(nextNode.x - m_Character.transform.position.x);
 		}
 
-		//m_Hero.Move(nextNode.x - m_Hero.transform.position.x);
-		m_Character.Move(nextNode.x < transform.position.x ? -1 : 1);
+		var direction = (nextNode - (Vector2)m_Character.transform.position).normalized;
 
-		var direction = (nextNode - (Vector2)transform.position).normalized;
-
-		if (Vector2.Angle(Vector2.up, direction) < m_JumpAngle)
+		if (m_JumpCooldownTimer <= 0 && Vector2.Angle(Vector2.up, direction) < m_JumpAngle)
 		{
-			m_Character.Jump();
+			m_Character.JumpTo(nextNode);
+			m_JumpCooldownTimer = m_JumpCooldown;
 		}
 
 		if (Vector2.Angle(Vector2.down, direction) < m_DropAngle)
@@ -88,18 +129,9 @@ public class Hero : MonoBehaviour
 		}
 	}
 
-	private IEnumerator Seek()
+	private void UpdatePath()
 	{
-		while (true)
-		{
-			var path = m_Seeker.StartPath(transform.position, m_Player.transform.position);
-
-			yield return StartCoroutine(path.WaitForPath());
-
-			m_Path = path.vectorPath.Select(x => (Vector2)x).ToList();
-
-			yield return new WaitForSeconds(0.5f);
-		}
+		m_Seeker.StartPath(transform.position, m_Target); 
 	}
 
 	private void OnLaserAttack()
@@ -114,12 +146,18 @@ public class Hero : MonoBehaviour
 
 	private void OnDrawGizmos()
 	{
-		if (m_Path == null || m_Path.Count == 0)
+		//Gizmos.color = Color.green;
+		//Gizmos.DrawWireSphere((Vector3)GetFurthestNode().position, 1);
+
+		if (m_Path.Count == 0)
 		{
 			return;
 		}
 
 		Gizmos.color = Color.yellow;
 		Gizmos.DrawWireSphere(m_Path.First(), 1);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(m_Path.Last(), 1);
 	}
 }
